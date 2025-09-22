ab3fad3b6fc1714b417d5d2c42d43d05fac6b314
camc2220 <camc2220@gmail.com>
2025-09-21 21:14:35 -0400

    dinamics ids

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -19,15 +19,15 @@ namespace EcommerceBackend.Data
         {
             base.OnModelCreating(modelBuilder);
 
-            // --- GUIDs fijos ---
-            var adminId = Guid.Parse("11111111-1111-1111-1111-111111111111");
-            var userId = Guid.Parse("22222222-2222-2222-2222-222222222222");
-            var product1Id = Guid.Parse("33333333-3333-3333-3333-333333333333");
-            var product2Id = Guid.Parse("44444444-4444-4444-4444-444444444444");
-            var cartItemId = Guid.Parse("55555555-5555-5555-5555-555555555555");
-            var invoiceId = Guid.Parse("66666666-6666-6666-6666-666666666666");
-            var invoiceItemId = Guid.Parse("77777777-7777-7777-7777-777777777777");
-            var paymentId = Guid.Parse("88888888-8888-8888-8888-888888888888");
+            // --- GUIDs generados dinámicamente ---
+            var adminId = Guid.NewGuid();
+            var userId = Guid.NewGuid();
+            var product1Id = Guid.NewGuid();
+            var product2Id = Guid.NewGuid();
+            var cartItemId = Guid.NewGuid();
+            var invoiceId = Guid.NewGuid();
+            var invoiceItemId = Guid.NewGuid();
+            var paymentId = Guid.NewGuid();
 
             // --- Usuarios ---
             modelBuilder.Entity<User>().HasData(
