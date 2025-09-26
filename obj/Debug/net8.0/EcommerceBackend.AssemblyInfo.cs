9bc17176e1a25f66ac6cad7410d4fdaba957cc13
camc2220 <camc2220@gmail.com>
2025-09-25 20:36:21 -0400

    1

--- obj/Debug/net8.0/EcommerceBackend.AssemblyInfo.cs
+++ obj/Debug/net8.0/EcommerceBackend.AssemblyInfo.cs
@@ -13,7 +13,6 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7ff40c631404036ef78b8627d2a513e6542cb31")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
